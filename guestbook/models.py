de1a08702b454d9f33b94358db99b2dd22ba6fe9
thanglna <thanglna@aoi-sys.vn>
2017-04-27 15:21:31 +0700

    Fix code style and add user login

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,32 +1,36 @@
 from google.appengine.ext import ndb
 from google.appengine.datastore.datastore_query import Cursor
+
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    '''Constructs a Datastore key for a Guestbook entity with guestbook_name.'''
-    return ndb.Key('Guestbook', guestbook_name)
+	'''Constructs a Datastore key for a Guestbook entity with guestbook_name.'''
+	return ndb.Key('Guestbook', guestbook_name)
+
 
 class Greeting(ndb.Model):
-    '''Models an individual Guestbook entry.'''
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+	'''Models an individual Guestbook entry.'''
+	author = ndb.UserProperty()
+	content = ndb.StringProperty(indexed=False)
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
-    @classmethod
-    def get_greeting_by_page(self, guestbook_name, urlsafe=''):
-        nums_of_page = 5
-        cursor = Cursor(urlsafe=urlsafe)
-        datas, next_cursor, more = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(
-            -Greeting.date).fetch_page(nums_of_page, start_cursor=cursor)
-        next_urlsafe = ''
-        if next_cursor is not None:
-            next_urlsafe = next_cursor.urlsafe
-        return datas, next_urlsafe, more
+	@classmethod
+	def get_greeting_by_page(self, guestbook_name, urlsafe=''):
+		nums_of_page = 5
+		cursor = Cursor(urlsafe=urlsafe)
+		datas, next_cursor, more = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(
+			-Greeting.date).fetch_page(nums_of_page, start_cursor=cursor)
+		next_urlsafe = ''
+		if next_cursor is not None:
+			next_urlsafe = next_cursor.urlsafe
+		return datas, next_urlsafe, more
 
-    @classmethod
-    @ndb.transactional
-    def insert_greeting(cls, guestbook_name, content):
-        greeting = cls(parent=guestbook_key(guestbook_name))
-        greeting.content = content
-        greeting.put()
-        return greeting
+	@classmethod
+	@ndb.transactional
+	def insert_greeting(cls, guestbook_name, author=None, content=''):
+		greeting = cls(parent=guestbook_key(guestbook_name))
+		if author:
+			greeting.author = author
+		greeting.content = content
+		greeting.put()
+		return greeting
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,77 +1,89 @@
 from django.views.generic import TemplateView, FormView
 from django.core.urlresolvers import reverse_lazy
+from google.appengine.api import users
 
 from guestbook.models import Greeting, DEFAULT_GUESTBOOK_NAME
 from guestbook.forms import SignForm
 
+
 class IndexView(TemplateView):
-    template_name = 'guestbook/main_page.html'
-    context_object_name = 'greetings'
+	template_name = 'guestbook/main_page.html'
+	context_object_name = 'greetings'
 
-    def get_guestbook_name(self):
-        guestbook_name = self.request.GET.get('guestbook_name')
-        if not guestbook_name or guestbook_name == '':
-            guestbook_name = DEFAULT_GUESTBOOK_NAME
-        return guestbook_name
+	def get_guestbook_name(self):
+		guestbook_name = self.request.GET.get('guestbook_name')
+		if not guestbook_name or guestbook_name == '':
+			guestbook_name = DEFAULT_GUESTBOOK_NAME
+		return guestbook_name
 
-    def get_context_data(self, **kwargs):
-        guestbook_name = self.get_guestbook_name()
-        urlsafe = self.request.GET.get('urlsafe', '')
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['guestbook_list'], context['urlsafe'], context['more'] = \
-            Greeting.get_greeting_by_page(guestbook_name, urlsafe)
-        context['url_linktext'] = 'Login'
-        context['guestbook_name'] = guestbook_name
+	def get_context_data(self, **kwargs):
+		guestbook_name = self.get_guestbook_name()
+		urlsafe = self.request.GET.get('urlsafe', '')
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['guestbook_list'], context['urlsafe'], context['more'] = \
+			Greeting.get_greeting_by_page(guestbook_name, urlsafe)
+		# context['url_linktext'] = 'Login'
+		context['guestbook_name'] = guestbook_name
 
-        return context
+		return context
 
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		if users.get_current_user():
+			url = users.create_logout_url(request.get_full_path())
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(request.get_full_path())
+			url_linktext = 'Login'
+
+		context['url'] = url
+		context['url_linktext'] = url_linktext
+		return self.render_to_response(context)
+
 
 class SignView(FormView):
-    template_name = 'guestbook/sign_page.html'
-    form_class = SignForm
-    success_url = reverse_lazy('index')
+	template_name = 'guestbook/sign_page.html'
+	form_class = SignForm
+	success_url = reverse_lazy('index')
 
-    def sign_book(self, guestbook_name, content):
-        if guestbook_name != '':
-            greeting = Greeting.insert_greeting(guestbook_name, content)
+	def sign_book(self, guestbook_name, content):
+		if guestbook_name != '':
+			greeting = Greeting.insert_greeting(guestbook_name, users.get_current_user(), content)
 
-    def get_guestbook_name(self):
-        guestbook_name = self.request.GET.get('guestbook_name', '')
-        return guestbook_name
+	def get_guestbook_name(self):
+		guestbook_name = self.request.GET.get('guestbook_name', '')
+		return guestbook_name
 
-    def get_initial(self):
-        initial = super(SignView, self).get_initial()
-        initial['guestbook_name'] = self.get_guestbook_name()
-        return initial
+	def get_initial(self):
+		initial = super(SignView, self).get_initial()
+		initial['guestbook_name'] = self.get_guestbook_name()
+		return initial
 
-    def get_context_data(self, **kwargs):
-        guestbook_name = self.get_guestbook_name()
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        context = super(SignView, self).get_context_data(**kwargs)
-        context['guestbook_name'] = guestbook_name
-        context['sign_form'] = form
-        return context
+	def get_context_data(self, **kwargs):
+		guestbook_name = self.get_guestbook_name()
+		form_class = self.get_form_class()
+		form = self.get_form(form_class)
+		context = super(SignView, self).get_context_data(**kwargs)
+		context['guestbook_name'] = guestbook_name
+		context['sign_form'] = form
+		return context
 
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		return self.render_to_response(context)
 
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+	def post(self, request, *args, **kwargs):
+		form_class = self.get_form_class()
+		form = self.get_form(form_class)
+		if form.is_valid():
+			return self.form_valid(form)
+		else:
+			return self.form_invalid(form)
 
-    def form_valid(self, form, **kwargs):
-        self.sign_book(form.cleaned_data['guestbook_name'], form.cleaned_data['content'])
-        return super(SignView, self).form_valid(form, **kwargs)
+	def form_valid(self, form, **kwargs):
+		self.sign_book(form.cleaned_data['guestbook_name'], form.cleaned_data['content'])
+		return super(SignView, self).form_valid(form, **kwargs)
 
-    def get_success_url(self):
-        url = reverse_lazy('index')
-        return '%s?guestbook_name=%s' % (url, self.get_guestbook_name())
+	def get_success_url(self):
+		url = reverse_lazy('index')
+		return '%s?guestbook_name=%s' % (url, self.get_guestbook_name())
